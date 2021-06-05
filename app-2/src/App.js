cf83cc4ad1cf14d9544acab1b2ceab430687b412
onava84 <oscar_nf@hotmail.com>
2021-06-05 18:00:37 -0500

    finished

--- app-2/src/App.js
+++ app-2/src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react'
 import './App.css';
+import axios from 'axios' 
+import Character from './Character'
 
 function App() {
+const [characters, setCharacters] = useState([])
+
+const getStarWarsCharacter = async () => {
+  const response = await axios.get(
+    `https://swapi.dev/api/people/`
+  );
+  setCharacters(response.data.results);
+};
+
+useEffect(() => {
+  getStarWarsCharacter()
+}, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {characters.map((e,i) => {
+        return <Character name={e.name} key={i}/>
+      })}
     </div>
   );
 }
